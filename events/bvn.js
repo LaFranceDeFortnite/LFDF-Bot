01c728222980c22056ab8c7ead18ba4355ad1987
P M L O <48804005+pmlo@users.noreply.github.com>
2019-03-27 19:12:02 +0100

    Update bvn.js

--- events/bvn.js
+++ events/bvn.js
@@ -63,7 +63,7 @@ function addOrRemoveRole(t, member, role) {
 client.on('guildMemberAdd', member =>{
     let embed = new Discord.RichEmbed()
         .setColor('RANDOM')
-        .setDescription('Hey ' + member.toString() + " , bienvenue sur **La France De Fortnite** :tada: :hugging: !\n\n N'oublie pas de choisir t'es grade dans ➔ <#515850558020517898> \n\n Et de mettre dans la Boutique Fortnite notre code Créateur : **LFDF-TOURNOI** :gem:")
+        .setDescription(`Hey ${member.user} , bienvenue sur **La France De Fortnite** :tada: :hugging: !\n\n N'oublie pas de choisir t'es grade dans ➔ <#515850558020517898> \n\n Et de mettre dans la Boutique Fortnite notre code Créateur : **LFDF-TOURNOI** :gem:`)
         .setFooter('Nous sommes désormais ' + member.guild.memberCount)
     member.guild.channels.get('519294938102366250').send(embed)
     member.addRole('533994115063611402')
