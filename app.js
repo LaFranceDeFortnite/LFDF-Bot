fd9c406139f0e04a6de4975c697d971dee538029
P M L O <48804005+pmlo@users.noreply.github.com>
2019-04-06 22:36:53 +0200

    Update app.js

--- app.js
+++ app.js
@@ -162,10 +162,13 @@ bot.connectDatabase = function connectDatabase(bot, mongoose) {
 };
 
 bot.on("ready", () => {
-    console.log("Je suis prêt !");
-    bot.user.setActivity("Mp for Support", "https://www.twitch.tv/lafrancedefortnite");
+    let statuses = ["Mp for Support", `Mp for Support`];
+
+    setInterval(function() {
+        let status = statuses[Math.floor(Math.random() * statuses.length)];
+        bot.user.setActivity(status, {type: "STREAMING", url:"https://www.twitch.tv/lafrancedefortnite"});
+    }, 5000)
     bot.channels.get("540107675397128202").send(`:white_check_mark: Bot **${bot.user.username}** en ligne, Prêt à travailler !`);
-    bot.channels.get("540107675397128202").send(":wavy_dash: :wavy_dash: :wavy_dash: :black_small_square: :wavy_dash: :wavy_dash: :wavy_dash:")
 });
 
 bot.login(process.env.token);
