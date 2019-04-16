17e3c21905cd932a510bdfca8bb9fd7058451ad3
P M L O <48804005+pmlo@users.noreply.github.com>
2019-04-16 12:11:53 +0200

    Update role.js

--- events/role.js
+++ events/role.js
@@ -263,45 +263,5 @@ module.exports = (bot) => {
         console.log('Commande Role WEB TV');
         bot.channels.get("540107675397128202").send("Log / Utilisateur **" + message.author.username + "** / Commande Role **WEB TV**");
         }
-        if(message.content === "!b") {
-            let role = message.guild.roles.find('name', '【⚙】Staff')
-            let myRole = message.guild.roles.find(role => role.name === "【⚙】Resp.staff");
-            if(message.member.roles.find('name', '【⚙】Resp.staff')) {
-                message.member.removeRole(role)
-            var embed = new Discord.RichEmbed()
-                .setColor('cca25b')
-                .setDescription("Tu viens de quitter le groupe " + myRole)
-            message.channel.send(embed);
-            }
-            else {
-                message.member.addRole(role)
-                var embed = new Discord.RichEmbed()
-                    .setColor('cca25b')
-                    .setDescription("Tu as bien rejoint le groupe " + myRole)
-                message.channel.send(embed);
-            }
-            console.log('Commande Role PC');
-            bot.channels.get("540107675397128202").send("Log / Utilisateur **" + message.author.username + "** / Commande Role **PC**");
-        }
-        if(message.content === "!a") {
-            let role = message.guild.roles.find('name', '【📌】Resp. Staff')
-            let myRole = message.guild.roles.find(role => role.name === "【📌】Resp. Staff");
-            if(message.member.roles.find('name', '【📌】Resp. Staff')) {
-                message.member.removeRole(role)
-            var embed = new Discord.RichEmbed()
-                .setColor('cca25b')
-                .setDescription("Tu viens de quitter le groupe " + myRole)
-            message.channel.send(embed);
-            }
-            else {
-                message.member.addRole(role)
-                var embed = new Discord.RichEmbed()
-                    .setColor('cca25b')
-                    .setDescription("Tu as bien rejoint le groupe " + myRole)
-                message.channel.send(embed);
-            }
-            console.log('Commande Role PC');
-            bot.channels.get("540107675397128202").send("Log / Utilisateur **" + message.author.username + "** / Commande Role **PC**");
-        }
     });
 };
