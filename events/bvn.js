277ad9496e92f32ffa2b714e0d3cb0ceb2dc4307
P M L O <48804005+pmlo@users.noreply.github.com>
2019-05-28 20:42:32 +0200

    Update bvn.js

--- events/bvn.js
+++ events/bvn.js
@@ -12,4 +12,16 @@ client.on('guildMemberAdd', member =>{
     member.addRole('533995952671948811')
     member.addRole('515606140638330890')
     });
+client.on('guildMemberAdd', member => {
+    member.createDM().then(channel => {
+        let bvnembed = new Discord.RichEmbed()
+        .setColor('#10d64f')
+        .setTitle('Bienvenue sur le discord **La France De Fortnite**')
+        .setDescription(`N'oublie pas de choisir t'es grade dans ➔ <#515850558020517898> \n\n Et de mettre dans la Boutique Fortnite notre code Créateur : \n **LFDF-TOURNOI** \n\n __Nos réseaux :__ \n\n [Twich](https://www.twitch.tv/lafrancedefortnite) \n [Twitter](https://twitter.com/LaFranceDeFort1) \n [YouTuBe](https://www.youtube.com/channel/UC0KPlzbTKQhZzHITyljYotw) \n [Instagram](https://www.instagram.com/lafrancedefortnite/) \n [LFDF Animation](https://discord.gg/c65ZJSg)`)
+        .setThumbnail("https://cdn.discordapp.com/attachments/516014468086628352/582994096294199306/PicsArt_05-12-07.45.35.jpg")
+        .setFooter("Si tu as une question, renvoie-moi un message")
+        .setTimestamp()
+        return channel.send(bvnembed)
+    });
+    });
 };
