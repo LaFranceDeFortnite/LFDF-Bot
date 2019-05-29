172209bd00ef3b27465e2084aa6762ada529ef26
P M L O <48804005+pmlo@users.noreply.github.com>
2019-05-29 15:06:39 +0200

    Update logs.js

--- events/logs.js
+++ events/logs.js
@@ -60,21 +60,6 @@ module.exports = (client) => {
         }
     });
 
-    client.on('messageUpdate', async function(oldMessage, newMessage) {
-        if(oldMessage.content !== newMessage.content && !newMessage.author.bot){
-            let log = new Discord.RichEmbed()
-                .setColor(16753920)
-                .setAuthor(newMessage.author.username, newMessage.author.avatarURL)
-                .setDescription(newMessage.author.toString() + " a modifié son message")
-                .addField('Ancien Message', (oldMessage && oldMessage.content != null && oldMessage.content !== '')?oldMessage.content:"*Image/Embed*")
-                .addField('Nouveau Message', (newMessage.content)?newMessage.content:"*Image/Embed*")
-                .addField('ID', newMessage.id, true)
-                .addField('Salon', newMessage.channel.toString(), true)
-                .setTimestamp(new Date());
-            if(newMessage.member.guild.channels.find((channel) => channel.name === "logs"))newMessage.member.guild.channels.find((channel) => channel.name === "logs").send(log);
-        }
-    });
-
     client.on('messageDelete', async function(message) {
         const entry = await message.guild.fetchAuditLogs({type: 'MESSAGE_DELETE'}).then(audit => audit.entries.first());
         let user;
