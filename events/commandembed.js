ebfa589b141d307654d95f440273d19ed61b1c8b
P M L O <48804005+pmlo@users.noreply.github.com>
2020-01-09 20:45:25 +0100

    Update commandembed.js

--- events/commandembed.js
+++ events/commandembed.js
@@ -22,7 +22,7 @@ module.exports = (client) => {
             .setTimestamp()
             message.channel.send(fun_embed);
             console.log('Commande Fun');
-            client.channels.get("540107675397128202").send("Log / Utilisateur **" + message.author.username + "** / Commande **Fun**");
+            client.channels.get("664916311838687242").send("Log / Utilisateur **" + message.author.username + "** / Commande **Fun**");
         }
         if(message.content === "!help") {
             var fun_embed = new Discord.RichEmbed()
@@ -35,7 +35,7 @@ module.exports = (client) => {
             .setTimestamp()
             message.channel.send(fun_embed);
             console.log('Commande Help');
-            client.channels.get("540107675397128202").send("Log / Utilisateur **" + message.author.username + "** / Commande **Help**");
+            client.channels.get("664916311838687242").send("Log / Utilisateur **" + message.author.username + "** / Commande **Help**");
         }
         if(message.content === prefix + "modo") {
             var mod_embed = new Discord.RichEmbed()
@@ -49,7 +49,7 @@ module.exports = (client) => {
             .setTimestamp()
              message.channel.send(mod_embed);
              console.log('Commande Modo');
-             client.channels.get("540107675397128202").send("Log / Utilisateur **" + message.author.username + "** / Commande **Modo**");
+             client.channels.get("664916311838687242").send("Log / Utilisateur **" + message.author.username + "** / Commande **Modo**");
         }
         if(message.content === prefix + "reseaux") {
             var mod_embed = new Discord.RichEmbed()
@@ -61,7 +61,7 @@ module.exports = (client) => {
             .setTimestamp()
              message.channel.send(mod_embed);
              console.log('Commande Réseaux');
-             client.channels.get("540107675397128202").send("Log / Utilisateur **" + message.author.username + "** / Commande **Réseaux**");
+             client.channels.get("664916311838687242").send("Log / Utilisateur **" + message.author.username + "** / Commande **Réseaux**");
         }
         if(message.content === prefix + "serveur-info") {
             var info_embed = new Discord.RichEmbed()
@@ -75,7 +75,7 @@ module.exports = (client) => {
             .setFooter("Serveur : uFc4 Esport | ID : 515604887686610944")
             message.channel.sendMessage(info_embed);
             console.log('Commande Info');
-            client.channels.get("540107675397128202").send("Log / Utilisateur **" + message.author.username + "** / Commande **Info**");
+            client.channels.get("664916311838687242").send("Log / Utilisateur **" + message.author.username + "** / Commande **Info**");
         }
         if(message.content === prefix + "projet") {
             var info_embed = new Discord.RichEmbed()
@@ -83,7 +83,7 @@ module.exports = (client) => {
             .setImage("https://images-ext-2.discordapp.net/external/kIoFsV999Mm50aBO1ixMhoVtD-brAwOH1i07tOrf5G4/http/i0.kym-cdn.com/photos/images/newsfeed/001/246/478/eac.gif")
             message.channel.sendMessage(info_embed);
             console.log('Commande Projet');
-            client.channels.get("540107675397128202").send("Log / Utilisateur **" + message.author.username + "** / Commande **Projet**");
+            client.channels.get("664916311838687242").send("Log / Utilisateur **" + message.author.username + "** / Commande **Projet**");
         }
         if(message.content === prefix + "ah") {
             var info_embed = new Discord.RichEmbed()
@@ -91,7 +91,7 @@ module.exports = (client) => {
             .setImage("https://images-ext-2.discordapp.net/external/QLLPFggtUq-gOu1XbmsGRhVPrlQCtLCkFz4t3WgoRRs/%3Fitemid%3D7256068/https/media1.tenor.com/images/dfd5671e5d4847a48be0d024abd03e72/tenor.gif")
             message.channel.sendMessage(info_embed);
             console.log('Commande Ah');
-            client.channels.get("540107675397128202").send("Log / Utilisateur **" + message.author.username + "** / Commande **Ah (Deny)**");
+            client.channels.get("664916311838687242").send("Log / Utilisateur **" + message.author.username + "** / Commande **Ah (Deny)**");
         }
         if(message.content === prefix + "issou") {
             var info_embed = new Discord.RichEmbed()
@@ -99,7 +99,7 @@ module.exports = (client) => {
             .setImage("https://images-ext-2.discordapp.net/external/OshEMVAVJD71nF9uFm1MXe16DXWt05NAaDnGqm1azsw/%3Fitemid%3D7315327/https/media1.tenor.com/images/0ca6f8a1d31646286b94671f0cd0f5a4/tenor.gif")
             message.channel.sendMessage(info_embed);
             console.log('Commande Ah');
-            client.channels.get("540107675397128202").send("Log / Utilisateur **" + message.author.username + "** / Commande **Ah (Deny)**");
+            client.channels.get("664916311838687242").send("Log / Utilisateur **" + message.author.username + "** / Commande **Ah (Deny)**");
         }
         if(message.content.startsWith(prefix + "sayembed")) {
             let arg = message.content.split(" ").slice(1);
@@ -108,7 +108,7 @@ module.exports = (client) => {
                 .setDescription(thingToEco)
             message.channel.sendMessage(embed);
         message.delete();
-        client.channels.get("540107675397128202").send("Log / Utilisateur **" + message.author.username + "** / Commande **Say Embed**");
+        client.channels.get("664916311838687242").send("Log / Utilisateur **" + message.author.username + "** / Commande **Say Embed**");
         }
     });
 };
