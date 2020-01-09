72cd469115514eac08947dd099004827172795cb
P M L O <48804005+pmlo@users.noreply.github.com>
2020-01-09 20:41:23 +0100

    Update bvn.js

--- events/bvn.js
+++ events/bvn.js
@@ -7,10 +7,10 @@ client.on('guildMemberAdd', member =>{
         .setColor('RANDOM')
         .setDescription('Hey __**' + member.user.username + "**__, bienvenue sur **uFc4 Esport** :tada: :hugging: !\n\n N'oublie pas de choisir t'es grade dans ➔ <#515850558020517898> \n\n Et de mettre dans la Boutique Fortnite notre code Créateur : **UFC4-TEAM** :gem:")
         .setFooter('Nous sommes désormais ' + member.guild.memberCount)
-    member.guild.channels.get('519294938102366250').send(embed)
+    member.guild.channels.get('661697039960375296').send(embed)
+    member.addRole('515606140638330890')
     member.addRole('533994115063611402')
     member.addRole('533995952671948811')
-    member.addRole('515606140638330890')
     });
 client.on('guildMemberAdd', member => {
     member.createDM().then(channel => {
